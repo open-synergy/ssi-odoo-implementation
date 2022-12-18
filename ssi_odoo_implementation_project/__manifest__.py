81d69e6b8ff7dfc994ec5b9bf178cb8859adc78e
Andhitia Rama <andhitia.r@gmail.com>
2022-12-18 15:56:52 +0700

    [ssi_odoo_implementation_project] 14.0.1.0.0

---
+++ ssi_odoo_implementation_project/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Odoo Implementation - Project Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_odoo_implementation",
+        "ssi_project",
+    ],
+    "data": [
+        "views/odoo_implementation_views.xml",
+    ],
+    "demo": [],
+}
