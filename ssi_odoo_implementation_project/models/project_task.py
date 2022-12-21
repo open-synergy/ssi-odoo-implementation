25173c1dab775e9c21a800fc94f27878c1cc4dca
Andhitia Rama <andhitia.r@gmail.com>
2022-12-21 14:39:07 +0700

    [ssi_odoo_implementation_project] 14.0.1.3.0

--- ssi_odoo_implementation_project/models/project_task.py
+++ ssi_odoo_implementation_project/models/project_task.py
@@ -13,7 +13,7 @@ class ProjectTask(models.Model):
 
     direct_feature_implementation_ids = fields.Many2many(
         string="Direct Feature Implementation",
-        comodel_name="project.task",
+        comodel_name="odoo_feature_implementation",
         relation="rel_feature_implementation_2_direct_task",
         column1="task_id",
         column2="feature_implementation_id",
