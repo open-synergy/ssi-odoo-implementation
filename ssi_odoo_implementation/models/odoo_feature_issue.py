77a99b84a2eece498fe383e1a824a642c4c3e7c7
Andhitia Rama <andhitia.r@gmail.com>
2025-06-26 03:24:33 +0700

    [UPD] ssi_odoo_implementation

--- ssi_odoo_implementation/models/odoo_feature_issue.py
+++ ssi_odoo_implementation/models/odoo_feature_issue.py
@@ -4,49 +4,53 @@
 
 from odoo import api, fields, models
 
+from odoo.addons.ssi_decorator import ssi_decorator
+
 
 class OdooFeatureIssue(models.Model):
     _name = "odoo_feature_issue"
     _inherit = [
+        "mixin.transaction_cancel",
+        "mixin.transaction_done",
         "mixin.transaction_confirm",
         "mixin.transaction_open",
-        "mixin.transaction_done",
-        "mixin.transaction_cancel",
+        "mixin.transaction_ready",
         "mixin.task",
     ]
     _description = "Odoo Feature Issue"
-    _approval_from_state = "draft"
-    _approval_to_state = "open"
+    _approval_from_state = "open"
+    _approval_to_state = "done"
     _approval_state = "confirm"
-    _after_approved_method = "action_open"
+    _after_approved_method = "action_done"
 
     # Attributes related to add element on view automatically
     _automatically_insert_view_element = True
+    _automatically_insert_done_policy_fields = False
+    _automatically_insert_done_button = False
 
-    # Attributes related to add element on form view automatically
-    _automatically_insert_open_policy_fields = False
-    _automatically_insert_open_button = False
     _task_create_page = True
     _task_page_xpath = "//page[2]"
     _task_template_position = "before"
 
-    _statusbar_visible_label = "draft,confirm,open,done"
+    _statusbar_visible_label = "draft,ready,open,confirm,done"
 
     _policy_field_order = [
+        "ready_ok",
+        "open_ok",
         "confirm_ok",
         "approve_ok",
         "reject_ok",
         "restart_approval_ok",
         "cancel_ok",
-        "done_ok",
         "restart_ok",
         "manual_number_ok",
     ]
     _header_button_order = [
+        "action_ready",
+        "action_open",
         "action_confirm",
         "action_approve_approval",
         "action_reject_approval",
-        "action_done",
         "%(ssi_transaction_cancel_mixin.base_select_cancel_reason_action)d",
         "action_restart",
     ]
@@ -54,14 +58,15 @@ class OdooFeatureIssue(models.Model):
     # Attributes related to add element on search view automatically
     _state_filter_order = [
         "dom_draft",
+        "dom_ready",
+        "dom_open",
         "dom_confirm",
         "dom_reject",
-        "dom_open",
         "dom_done",
         "dom_cancel",
     ]
 
-    _create_sequence_state = "open"
+    _create_sequence_state = "ready"
 
     date = fields.Date(
         string="Date",
@@ -97,21 +102,6 @@ class OdooFeatureIssue(models.Model):
         readonly=True,
         states={"draft": [("readonly", False)]},
     )
-    state = fields.Selection(
-        string="State",
-        selection=[
-            ("draft", "Draft"),
-            ("confirm", "Waiting for Approval"),
-            ("open", "In Progress"),
-            ("done", "Done"),
-            ("reject", "Rejected"),
-            ("cancel", "Cancelled"),
-        ],
-        copy=False,
-        default="draft",
-        required=True,
-        readonly=True,
-    )
 
     @api.model
     def _default_date(self):
@@ -119,17 +109,24 @@ class OdooFeatureIssue(models.Model):
 
     @api.model
     def _get_policy_field(self):
-        res = super(OdooFeatureIssue, self)._get_policy_field()
+        res = super()._get_policy_field()
         policy_field = [
+            "ready_ok",
+            "open_ok",
             "confirm_ok",
             "approve_ok",
             "reject_ok",
-            "open_ok",
+            "restart_approval_ok",
             "done_ok",
             "cancel_ok",
             "restart_ok",
-            "restart_approval_ok",
             "manual_number_ok",
         ]
         res += policy_field
         return res
+
+    @ssi_decorator.insert_on_form_view()
+    def _insert_form_element(self, view_arch):
+        if self._automatically_insert_view_element:
+            view_arch = self._reconfigure_statusbar_visible(view_arch)
+        return view_arch
